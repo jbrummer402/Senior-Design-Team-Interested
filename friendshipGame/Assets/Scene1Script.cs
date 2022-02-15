06467d47263451bd5317b801e786e7bd9808ee6c
gradymcd <gradyrmcd@gmail.com>
2022-02-15 14:15:02 -0500

    Made dialog format much simpler and less of a recurive nightmare.

--- friendshipGame/Assets/Scene1Script.cs
+++ friendshipGame/Assets/Scene1Script.cs
@@ -5,53 +5,52 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 using Newtonsoft.Json;
-using System.IO;
+using System;
 
 public class Scene1Script : MonoBehaviour
 {
-    public class Dialog 
+
+    public class Dialog
     {
-        private string path;
-        public string title;
-        public string response;
-        public string who;
-        public string ptr;
-        public Dialog[] options;
+        public class Entry
+        {
+            public class Option
+            {
+                public string id;
+                public string title;
+                public string response;
+            }
+
+            public string who;
+            public string response;
+            public IList<Option> options;
+        }
+
+        public int scene;
+        public IDictionary<string, Entry> entries;
 
         public static Dialog CreateFromJSON(string jsonString) {
-            Dialog d = JsonConvert.DeserializeObject<Dialog>(jsonString);
-            d.fillPaths("1");
-            return d;
+            return JsonConvert.DeserializeObject<Dialog>(jsonString);
         }
 
-        public static void fillPaths(Dialog d) {
-            if(d.options != null) {
-                for(int i=0; i<d.options.Length; ++i){
-                    d.options[i].path = d.path + "/" + (i+1);
-                    fillPaths(d.options[i]);
-                }
-            }
+        public Boolean HasEntry(string id) {
+            return entries.ContainsKey(id);
         }
 
-
-        public void fillPaths(string path) {
-            this.path = path;
-            fillPaths(this);
+        public Entry GetEntry(string id) {
+            if(!HasEntry(id))
+                return null;
+            return entries[id];
         }
 
-        public string GetPath() {
-            return this.path;
-        }
     }
-
-
 
     [Header("Scene Objects")]
     [SerializeField] private TMP_Text Response1Text = null;
     [SerializeField] private TMP_Text Response2Text = null;
-    [SerializeField] private TMP_Text Response3Text = null; 
+    [SerializeField] private TMP_Text Response3Text = null;
     [SerializeField] private Button ContinueBtn = null;
-    [SerializeField] private Button Response1Btn = null; 
+    [SerializeField] private Button Response1Btn = null;
     [SerializeField] private Button Response2Btn = null;
     [SerializeField] private Button Response3Btn = null;
     [SerializeField] private TMP_Text PromptText = null;
@@ -60,108 +59,106 @@ public class Scene1Script : MonoBehaviour
     private string FirstChar = "FirstChar";
     private string Scene1Pos = "1";
     private Queue<string> Sentences;
-    private Dialog rootDialog;
-
-    Dialog getDialog(string path) { 
-        //1-1-1 -> rootDialog.options[0].options[0]
-        Dialog d = rootDialog;
-        string[] pathArr = path.Split('/');
-        
-        for(int i=1; i<pathArr.Length; ++i) { // Starting at 1 to exclude the root path
-            d = d.options[int.Parse(pathArr[i])-1];
-        }
-
-        return d;
-    }
-
+    private Dialog dialog;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         ///TODO load from a file
         string jsonDialog = @"{
-	        ""title"":""Begin"",
-	        ""response"": ""Time to get dressed. What outfit would you like to wear?"",
-	        ""who"": "" "",
-	        ""options"":[
-		        {
-			        ""title"":""Tuxedo and a Suit"",
-			        ""response"": ""That seems a bit much for the occasion. A more casual outfit would be better suited.\fWhat outfit would you like to wear?"",
-			        ""ptr"": ""1""
-		        },
-		        {
-			        ""title"":""Jeans/khakis and a polo/button-up shirt"",
-                    ""response"": ""Good choice! Time to pack now"",
+	        ""scene"": 1,
+	        ""entries"": {
+		        ""start"": {
+			        ""response"": ""Time to get dressed. What outfit would you like to wear?"",
 			        ""options"": [
 				        {
-					        ""title"": ""[packing game placeholder]"",
-					        ""response"": ""It's time for breakfast. What do you want to eat?"",
-					        ""options"": [
-						        {
-							        ""title"": ""Fruit"",
-							        ""response"": ""That is a good and healthy choice. You feel ready to take on the day!"",
-								        ""options"": [
-									        {
-										        ""title"": ""[hygeine game]"",
-										        ""response"": ""It's time to go now. You're destination is 30 minutes away. When should you leave to arrive on time by 11:00"",
-										        ""options"": [
-											        {
-												        ""title"": ""10:20"",
-												        ""response"": ""You get there 10 minutes before everyone. It may be better to leave slightly later next time.""
-												
-											        },
-											        {
-												        ""title"": ""10:30"",
-												        ""response"": ""You show up on time and begin studying.""
-												
-											        },									
-											        {
-												        ""title"": ""10:40"",
-												        ""response"": ""You get there 10 minutes after everyone. It may be better to leave slightly earlier next time.""
-											        }
-										        ]
-									        }
-								        ]
-						        },
-						        {
-							        ""title"": ""Cracker"",
-							        ""response"": ""Fruit may have been a better option."",
-							        ""ptr"": ""1/2/1/1""
-						        },
-						        {
-							        ""title"": ""Candy bar"",
-							        ""response"": ""Fruit may have been a better option."",
-							        ""ptr"": ""1/2/1/1""
-						        }
-					        ]
+					        ""id"": ""start"",
+					        ""title"": ""Tuxedo and a suit"",
+					        ""response"": ""That seems a bit much for the occasion. A more casual outfit would be better suited.""
+				        },
+				        {
+					        ""id"": ""breakfast"",
+					        ""title"": ""Jeans/khakis and a polo/button-up shirt""
+				        },
+				        {
+					        ""id"": ""breakfast"",
+					        ""title"": ""Shorts and a t-shirt"",
+					        ""response"": ""It may be a better idea to be slightly better dressed for the occasion.""
 				        }
 			        ]
 		        },
-		        {
-			        ""title"":""Shorts and a t-shirt"",
-			        ""response"": ""It may be a better idea to be slightly better dressed for the occasion.\fTime to pack now"",
-			        ""ptr"": ""1/2""
+		        ""breakfast"": {
+			        ""response"": ""It's time for breakfast. What do you want to eat?"",
+			        ""options"": [
+				        {
+					        ""id"": ""packingGame"",
+					        ""title"": ""Fruit"",
+					        ""image"": ""apple"",
+					        ""response"": ""That is a good and healthy choice. You feel ready to take on the day!""
+				        },
+				        {
+					        ""id"": ""packingGame"",
+					        ""title"": ""Toast"",
+					        ""response"": ""That wasn't a bad choice, but the apple would have been a healthier option.""
+				        },
+				        {
+					        ""id"": ""breakfast"",
+					        ""title"": ""Candy bar"",
+					        ""response"": ""A healthier choice would be a better way to start your day.""
+				        }
+			        ]
+		        },
+		        ""packingGame"": {
+			        ""scene"": ""packingGame"",
+			        ""response"": ""Good job packing! Are you ready to brush your teeth now?"",
+			        ""options"": [
+				        {
+					        ""id"": ""hygeineGame"",
+					        ""title"": ""Yes!""
+				        }
+			        ]
+		        },
+		        ""hygeineGame"": {
+			        ""scene"": ""hygeineGame"",
+			        ""response"": ""You feel fresh and clean!\fYour destination is 30 minutes away. When should you leave to arrive on time by 11AM?"",
+			        ""options"": [
+				        {
+					        ""id"": ""end"",
+					        ""title"": ""10:20"",
+					        ""response"": ""You get there 10 minutes before everyone. You can leave a bit later and still arrive on time in the future.""
+				        },
+				        {
+					        ""id"": ""end"",
+                            ""title"": ""10:30"",
+					        ""response"": ""You show up on time and begin studying.""
+				        },
+				        {
+					        ""id"": ""end"",
+                            ""title"": ""10:40"",
+					        ""response"": ""You get there 10 minutes after everyone. It may be better to leave slightly earlier next time.""
+				        }
+			        ]
 		        }
-	        ]
+	        }
         }";
-        rootDialog = Dialog.CreateFromJSON(jsonDialog);
-        
-        CharName.text = rootDialog.who;
+        //rootDialog = Dialog.CreateFromJSON(jsonDialog);
+
+        dialog = Dialog.CreateFromJSON(jsonDialog);
+
+        //CharName.text = rootDialog.who;
         Sentences = new Queue<string>();
 
         Response1Btn.gameObject.SetActive(false);
         Response2Btn.gameObject.SetActive(false);
         Response3Btn.gameObject.SetActive(false);
-        
-        PlayerPrefs.SetString(Scene1Pos, "1");
 
-        queueDialogue(rootDialog);
-        
+        PlayerPrefs.SetString(Scene1Pos, "start");
+
+        queueDialog(dialog.GetEntry("start").response);
+
         ContinueDialogue();
     }
 
-    public void ContinueDialogue()
-    {
+    public void ContinueDialogue() {
         ContinueBtn.gameObject.SetActive(true);
         string currentLocation = PlayerPrefs.GetString(Scene1Pos);
         string sentence = Sentences.Dequeue();
@@ -169,24 +166,21 @@ public class Scene1Script : MonoBehaviour
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
 
-        if (Sentences.Count == 0)
-        {
+        if(Sentences.Count == 0) {
             ContinueBtn.gameObject.SetActive(false);
             DisplayButtons();
             return;
         }
 
-        
+
 
     }
 
-    IEnumerator TypeSentence(string sentence)
-    {
+    IEnumerator TypeSentence(string sentence) {
         PromptText.text = "";
-        foreach (char letter in sentence.ToCharArray())
-        {
+        foreach(char letter in sentence.ToCharArray()) {
             PromptText.text += letter;
-            for (int i = 0; i < 5; i++) {
+            for(int i = 0; i < 5; i++) {
                 yield return null;
             }
         }
@@ -198,96 +192,76 @@ public class Scene1Script : MonoBehaviour
         Response3Btn.gameObject.SetActive(false);
 
         string currentLocation = PlayerPrefs.GetString(Scene1Pos);
-       
 
-        ///TODO null check
-        Dialog d = getDialog(currentLocation);
-        ///TODO Support mre than 3 buttons
-        List<string> responses = new List<string>();
-        foreach(Dialog option in d.options) {
-            if(option.ptr != null && option.title == null)
-                responses.Add(getDialog(option.ptr).title);
-            else
-                responses.Add(option.title);
-            
+
+        Dialog.Entry e = dialog.GetEntry(currentLocation);
+        if(e == null) {
+            return;
+        }
+        ///TODO Support more than 3 buttons
+        List<string> buttonTitles = new List<string>();
+        foreach(Dialog.Entry.Option r in e.options) {
+            buttonTitles.Add(r.title);
         }
 
-        if(responses.Count > 0) {
+        if(buttonTitles.Count > 0) {
             Response1Btn.gameObject.SetActive(true);
-            Response1Text.text = responses[0];
+            Response1Text.text = buttonTitles[0];
 
         }
-        if(responses.Count > 1) {
+        if(buttonTitles.Count > 1) {
             Response2Btn.gameObject.SetActive(true);
-            Response2Text.text = responses[1];
+            Response2Text.text = buttonTitles[1];
         }
-        if(responses.Count > 2) {
+        if(buttonTitles.Count > 2) {
             Response3Btn.gameObject.SetActive(true);
-            Response3Text.text = responses[2];
+            Response3Text.text = buttonTitles[2];
         }
     }
 
-    public void ClickOne()
-    {
+    public void ClickOne() {
         ClickHandler(1);
     }
 
-    public void ClickTwo()
-    {
+    public void ClickTwo() {
         ClickHandler(2);
     }
 
-    public void ClickThree()
-    {
+    public void ClickThree() {
         ClickHandler(3);
     }
 
-    void ClickHandler(int Pressed)
-    {
+    void ClickHandler(int Pressed) {
         string currentLocation = PlayerPrefs.GetString(Scene1Pos);
         Response1Btn.gameObject.SetActive(false);
         Response2Btn.gameObject.SetActive(false);
         Response3Btn.gameObject.SetActive(false);
 
-        
-        currentLocation += "/" + Pressed;
-        Dialog d = getDialog(currentLocation);
+        Dialog.Entry entry = dialog.GetEntry(currentLocation);
+        Dialog.Entry.Option option = entry.options[Pressed - 1];
+        currentLocation = option.id;
 
-        queueDialogue(d);
-        
-        if(d.ptr != null) {
-            currentLocation = getDialog(d.ptr).GetPath();
+        if(option.response != null) {
+            queueDialog(option.response);
         }
-        
-        PlayerPrefs.SetString(Scene1Pos, currentLocation);
-        ContinueDialogue();
 
-        //ContinueDialogue();
+        PlayerPrefs.SetString(Scene1Pos, currentLocation);
+        if(dialog.HasEntry(currentLocation))
+            queueDialog(dialog.GetEntry(currentLocation).response);
+        ContinueDialogue();
     }
 
-    void queueDialogue(Dialog d)
-    {
-        /*if(d.ptr != null) {
-            d = getDialog(d.ptr);
-        }*/
-        if(d.who != "")
-            CharName.text = d.who;            
-        if(d.response == null || d.response == "") {
-            Sentences.Enqueue("A SENTENCE WAS NULL HOW DID THIS HAPPEN");
-        }
-        else foreach (string sentence in d.response.Split('\f'))
-        {
+    void queueDialog(string text) {
+        foreach(string sentence in text.Split('\f')) {
             Sentences.Enqueue(sentence);
         }
     }
 
-    void enterMicrogame(string gameScene)
-    {
+    void enterMicrogame(string gameScene) {
         SceneManager.LoadScene(gameScene);
     }
 
-    void incorrectAnswer(string reasoning)
-    {
-        
+    void incorrectAnswer(string reasoning) {
+
     }
 }
